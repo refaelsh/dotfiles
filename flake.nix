b1ac985aea2991433bcfc11c23e02da5d3dc9f71
refaelsh <refaelsh@pm.me>
2024-09-20 17:01:09 +0300

    WIP

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim.url = "./nixvim";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      nixosConfigurations = {
+        myNixos = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs system;
+          };
+
+          modules = [
+            ./configuration.nix
+            {
+              environment.systemPackages =[
+                inputs.nixvim.packages.${system}.default
+              ];
+            }
+          ];
+        };
+      };
+    };
+}
