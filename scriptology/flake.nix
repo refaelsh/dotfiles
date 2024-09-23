5e719cebb3af52baa473342080be1599d5213783
refaelsh <refaelsh@pm.me>
2024-09-23 22:48:00 +0300

    WIP

---
+++ scriptology/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A collection of shell scripts for NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        scripts = {
+          # Example script, replace or add your own scripts here
+          helloScript = pkgs.writeShellScriptBin "hello" ''
+            echo "Hello from Nix flake!"
+          '';
+        };
+      in
+      {
+        packages = {
+          default = pkgs.symlinkJoin {
+            name = "scripts";
+            paths = builtins.attrValues scripts;
+          };
+        };
+}
