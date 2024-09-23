bbeb7d2053328070fa966fbd1db57f531e306be3
refaelsh <refaelsh@pm.me>
2024-09-23 21:15:50 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -25,8 +25,9 @@
 
           modules = [
             ./nixos/configuration.nix
+            inputs.home-manager.nixosModules.home-manager
             # inputs.home-manager.nixosModules.home-manager
-            inputs.home-manager.homeConfigurations.refaelsh
+            # inputs.home-manager.homeConfigurations.refaelsh
             # inputs.home-manager.homeConfigurations.homeManagerModules.bbb
             # inputs.home-manager.homeManagerModules.bbb
             {
