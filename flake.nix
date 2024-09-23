b4e9e73bbd706b66cd7f2f794091aee02cec7a02
refaelsh <refaelsh@pm.me>
2024-09-23 23:14:09 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -7,9 +7,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # home-manager.url = "github:refaelsh/dotfiles?dir=bla";
+    home-manager.url = "github:refaelsh/dotfiles?dir=bla";
     nixvim.url = "github:refaelsh/dotfiles?dir=nixvim";
-    scriptology.url = "github:refaelsh/dotfiles?dir=scriptology";
+    # scriptology.url = "github:refaelsh/dotfiles?dir=scriptology";
   };
 
   outputs =
