5c8ded93e672765a40c36f283c97f92b223fa447
refaelsh <refaelsh@pm.me>
2024-09-23 21:13:57 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -31,10 +31,11 @@
             # inputs.home-manager.homeManagerModules.bbb
             {
               environment.systemPackages = [
-                # inputs.nixvim.packages.${system}.default
+                inputs.nixvim.packages.${system}.default
                 # inputs.home-manager.package.${system}.default
                 # inputs.home-manager.homeConfigurations.default
-                # inputs.home-manager.packages.${system}.default              ];
+                # inputs.home-manager.packages.default.x86_64-linux
+              ];
 
               # home-manager = {
               #   users.refaelsh = import ./home-manager/home.nix;
