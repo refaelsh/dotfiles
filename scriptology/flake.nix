d795208b454a3409f6a45dd21c3f181ff1c29c63
refaelsh <refaelsh@pm.me>
2024-09-23 22:49:42 +0300

    WIP

--- scriptology/flake.nix
+++ scriptology/flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         scripts = {
@@ -24,4 +30,6 @@
             paths = builtins.attrValues scripts;
           };
         };
+      }
+    );
 }
