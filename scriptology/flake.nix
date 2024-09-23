f34783db0a2d4399077bc1eee6d0fa145983d408
refaelsh <refaelsh@pm.me>
2024-09-23 23:03:10 +0300

    WIP

--- scriptology/flake.nix
+++ scriptology/flake.nix
@@ -5,15 +5,53 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    # This section defines the output for each system
-    packages.x86_64-linux.default = 
-      let 
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.runCommand "shell-scripts" {} ''
-          echo "sdfdsfds"
-      '';
+  outputs =
+    { self, nixpkgs }:
+    {
 
-  };
+      # NixOS module that can be imported in your NixOS configuration
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        {
+          imports = [ ];
+
+          config = {
+            environment.systemPackages = [
+              (pkgs.writeScriptBin "my-script" ''
+                #!/bin/sh
+                echo "Hello from my Nix packaged script!"
+              '')
+            ];
+          };
+        };
+
+      # If you want to make this script available outside of NixOS configuration, for example, for development or testing:
+      packages.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.writeShellApplication {
+          name = "git.sh";
+          text = # bash
+            ''
+              echo ----------------------------Running Git---------------------------------
+              git -C ~/repos/dotfiles add . && git -C ~/repos/dotfiles commit -m "WIP" && git -C ~/repos/dotfiles push
+              echo ----------------------------Finished running Git---------------------------------
+            '';
+        };
+    };
 }
+# (writeShellApplication {
+#   name = "git.sh";
+#   text = # bash
+#     ''
+#       echo ----------------------------Running Git---------------------------------
+#       git -C ~/repos/dotfiles add . && git -C ~/repos/dotfiles commit -m "WIP" && git -C ~/repos/dotfiles push
+#       echo ----------------------------Finished running Git---------------------------------
+#     '';
+# })
