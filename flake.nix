529c890ce75060fb9aedae1022390349b74fc455
refaelsh <refaelsh@pm.me>
2024-09-23 20:41:51 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    home-manager.url = "github:refaelsh/dotfiles?dir=bla";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # home-manager.url = "github:refaelsh/dotfiles?dir=bla";
     nixvim.url = "github:refaelsh/dotfiles?dir=nixvim";
   };
 
@@ -26,16 +26,16 @@
           modules = [
             ./nixos/configuration.nix
             # inputs.home-manager.homeConfigurations.bbb
-            inputs.home-manager.homeConfigurations.homeManagerModules.bbb
+            # inputs.home-manager.homeConfigurations.homeManagerModules.bbb
             # inputs.home-manager.homeManagerModules.bbb
             {
               environment.systemPackages = [
                 inputs.nixvim.packages.${system}.default
               ];
 
-              # home-manager = {
-              #   users.refaelsh = import ./home-manager/home.nix;
-              # };
+              home-manager = {
+                users.refaelsh = import ./home-manager/home.nix;
+              };
             }
           ];
         };
