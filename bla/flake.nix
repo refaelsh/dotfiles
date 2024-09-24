74c04c69f59ca415819a9a9822e50ac45bb3942a
refaelsh <refaelsh@pm.me>
2024-09-24 23:07:59 +0300

    WIP

--- bla/flake.nix
+++ bla/flake.nix
@@ -16,25 +16,25 @@
       home-manager,
     }:
     {
-      homeConfigurations = {
-        standalone = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Adjust the system if necessary
-          modules = [
-            ./home.nix # Your home-manager configuration file
-          ];
-        };
-      };
-
-      # If you want to make the home-manager module directly accessible:
-      packages.x86_64-linux.default = self.homeConfigurations.standalone.activationPackage;
-
-      # nixosConfigurations = {
-      #   myNixos = nixpkgs.lib.nixosSystem {
-      #     system = "x86_64-linux";
+      # homeConfigurations = {
+      #   standalone = home-manager.lib.homeManagerConfiguration {
+      #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Adjust the system if necessary
       #     modules = [
       #       ./home.nix # Your home-manager configuration file
       #     ];
       #   };
       # };
+
+      # If you want to make the home-manager module directly accessible:
+      packages.x86_64-linux.default = self.homeConfigurations.standalone.activationPackage;
+
+      nixosConfigurations = {
+        myNixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./home.nix # Your home-manager configuration file
+          ];
+        };
+      };
     };
 }
