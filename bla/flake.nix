c9fe76d2f773aae2003a77be116c2a6a5b1a0a08
refaelsh <refaelsh@pm.me>
2024-09-24 22:20:25 +0300

    WIP

--- bla/flake.nix
+++ bla/flake.nix
@@ -16,14 +16,14 @@
       home-manager,
     }:
     {
-      homeConfigurations = {
-        standalone = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Adjust the system if necessary
-          modules = [
-            ./home.nix # Your home-manager configuration file
-          ];
-        };
-      };
+      # homeConfigurations = {
+      #   standalone = home-manager.lib.homeManagerConfiguration {
+      #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Adjust the system if necessary
+      #     modules = [
+      #       ./home.nix # Your home-manager configuration file
+      #     ];
+      #   };
+      # };
 
       # If you want to make the home-manager module directly accessible:
       packages.x86_64-linux.default = self.homeConfigurations.standalone.activationPackage;
