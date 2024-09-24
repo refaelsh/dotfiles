485ac22f1615a0cb1903a143ee1c20386f5f6c3f
refaelsh <refaelsh@pm.me>
2024-09-24 22:36:05 +0300

    WIP

---
+++ home-manager/termonad.nix
@@ -0,0 +1,85 @@
+{
+  xdg.configFile."termonad/termonad.hs" = {
+    text = # haskell
+      ''
+        {-# LANGUAGE OverloadedStrings #-}  
+
+        module Main where
+
+        import Data.Maybe (fromMaybe)
+        import Termonad
+        import Termonad.Config.Colour
+
+        main :: IO ()
+        main = do
+        -- First, create the colour extension based on either PaperColor modules.
+        myColourExt <- createColourExtension dracula
+
+        -- Update 'myTMConfig' with our colour extension.
+        let newTMConfig = addColourExtension myTMConfig myColourExt
+
+        -- Start Termonad with our updated 'TMConfig'.
+        start newTMConfig
+
+        -- This is our Dracula 'ColourConfig'.
+        dracula :: ColourConfig (AlphaColour Double)
+        dracula =
+        defaultColourConfig
+        { -- Set the default background & foreground colour of text of the terminal.
+        backgroundColour = Set (createColour 40 42 54), -- black.0
+        foregroundColour = Set (createColour 248 248 242), -- white.7
+        -- Set the extended palette that has 2 Vecs of 8 Dracula palette colours
+        palette = ExtendedPalette draculaNormal draculaBright
+        }
+        where
+        draculaNormal :: List8 (AlphaColour Double)
+        draculaNormal =
+        fromMaybe defaultStandardColours $
+        mkList8
+        [ createColour 40 42 54, -- black.0
+        createColour 255 85 85, -- red.1
+        createColour 80 250 123, -- green.2
+        createColour 241 250 140, -- yellow.3
+        createColour 189 147 249, -- blue.4
+        createColour 255 121 198, -- magenta.5
+        createColour 139 233 253, -- cyan.6
+        createColour 191 191 191 -- white.7
+        ]
+
+        draculaBright :: List8 (AlphaColour Double)
+        draculaBright =
+        fromMaybe defaultStandardColours $
+        mkList8
+        [ createColour 77 77 77, -- black.8
+        createColour 255 110 103, -- red.9
+        createColour 90 247 142, -- green.10
+        createColour 244 249 157, -- yellow.11
+        createColour 202 169 250, -- blue.12
+        createColour 255 146 208, -- magenta.13
+        createColour 154 237 254, -- cyan.14
+        createColour 230 230 230 -- white.15
+        ]
+
+        -- This is our main 'TMConfig'.  It holds all of the non-colour settings
+        -- for Termonad.
+        --
+        -- This shows how a few settings can be changed.
+        myTMConfig :: TMConfig
+        myTMConfig =
+        defaultTMConfig
+        { options =
+        defaultConfigOptions
+        { showScrollbar = ShowScrollbarNever,
+        confirmExit = False,
+        showMenu = False,
+        cursorBlinkMode = CursorBlinkModeOn,
+        fontConfig =
+        defaultFontConfig
+        { fontFamily = "FiraCode Nerd Font",
+        fontSize = FontSizePoints 12
+        }
+        }
+        }
+      '';
+  };
+}
