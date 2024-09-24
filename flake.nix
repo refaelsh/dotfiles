723099275193b5bc1b9c75697b3e71e6d65ef388
refaelsh <refaelsh@pm.me>
2024-09-24 22:48:25 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           modules = [
             ./nixos/configuration.nix
             inputs.home-manager.nixosModules.home-manager
-            inputs.bbb.nixosConfigurations.myNixos
+            # inputs.bbb.nixosConfigurations.myNixos
             # inputs.home-manager.nixosModules.home-manager
             # inputs.home-manager.homeConfigurations.refaelsh
             # inputs.home-manager.nixosModules.home-manager
@@ -38,7 +38,7 @@
             {
               environment.systemPackages = [
                 inputs.nixvim.packages.${system}.default
-                # inputs.bbb.packages.${system}.default
+                inputs.bbb.packages.${system}.default
                 # inputs.home-manager.package.${system}.default
                 # inputs.home-manager.homeConfigurations.default
                 # inputs.home-manager.packages.default.x86_64-linux
