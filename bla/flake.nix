dfd377378b1e4a5e90d4e9ee3b6da1e9d2b3ac46
refaelsh <refaelsh@pm.me>
2024-09-24 20:46:41 +0300

    WIP

--- bla/flake.nix
+++ bla/flake.nix
@@ -9,39 +9,23 @@
     };
   };
 
-  # outputs =
-  #   {
-  #     self,
-  #     nixpkgs,
-  #     home-manager,
-  #     ...
-  #   }:
-  #   {
-  #     homeConfigurations."refaelsh" = home-manager.lib.homeManagerConfiguration {
-  #
-  #       # Your home-manager modules go here
-  #       modules = [
-  #         ./home.nix # Assuming your configuration is in home.nix
-  #         # Add other modules here
-  #       ];
-  #     };
-  #
-  #     packages.x86_64-linux.default =
-  #       self.homeConfigurations."refaelsh".config.home.packages.myPackage;
-  #   };
   outputs =
-    { nixpkgs, home-manager, ... }@inputs:
     {
-      homeManagerConfigurations.x86_64-linux = {
-        bbb = home-manager.lib.homeManagerConfiguration {
-          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    {
+      homeConfigurations."refaelsh" = home-manager.lib.homeManagerConfiguration {
 
-          modules = [
-            ./home.nix # Assuming your configuration is in home.nix
-            # Add other modules here
-          ];
-
-        };
+        # Your home-manager modules go here
+        modules = [
+          ./home.nix # Assuming your configuration is in home.nix
+          # Add other modules here
+        ];
       };
+
+      packages.x86_64-linux.default = self.homeConfigurations."refaelsh".config.home.packages.myPackage;
     };
 }
