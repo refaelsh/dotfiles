ad4fa4c4a1ec40e9f20bf5a51d3325946742dbf0
refaelsh <refaelsh@pm.me>
2024-09-25 18:35:45 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -21,11 +21,11 @@
 
           modules = [
             ./nixos/configuration.nix
-            inputs.home-manager.nixosModules.home-manager
+            # inputs.home-manager.nixosModules.home-manager
             {
               environment.systemPackages = [
                 inputs.nixvim.packages.${system}.default
-                # inputs.home-manager.packages.${system}.default
+                inputs.home-manager.packages.${system}.default
               ];
             }
           ];
