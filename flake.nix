cc884dbf665fda65aec3eccc92a9f3892b62967e
refaelsh <refaelsh@pm.me>
2024-09-25 23:23:18 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -29,7 +29,6 @@
             {
               environment.systemPackages = [
                 inputs.nixvim.packages.${system}.default
-                # inputs.home-manager.packages.${system}.default
               ];
             }
 
