99d792d6fc6c74b8078cd59960fd9556dfdd1f01
refaelsh <refaelsh@pm.me>
2024-09-26 17:55:07 +0300

    WIP

--- nixos/environment-variables.nix
+++ nixos/environment-variables.nix
@@ -6,13 +6,13 @@
     TERMINAL = "wezterm";
   };
 
-  environment.etc."brave/settings.json".text = lib.generators.toJSON { } {
-    # # This is a placeholder. You would map your settings here.
-    # homepage = config.brave.settings.homepage or "about:blank";
-    # # Example setting, might not correspond directly to about:config but illustrates the concept
-    # "browser.newtabpage.activity-stream.feeds.section.highlights" =
-    #   config.brave.settings.showHighlights or false;
-    # Example setting, might not correspond directly to about:config but illustrates the concept
-    "browser.newtabpage.activity-stream.feeds.section.highlights" = "true";
-  };
+  # environment.etc."brave/settings.json".text = lib.generators.toJSON { } {
+  #   # # This is a placeholder. You would map your settings here.
+  #   # homepage = config.brave.settings.homepage or "about:blank";
+  #   # # Example setting, might not correspond directly to about:config but illustrates the concept
+  #   # "browser.newtabpage.activity-stream.feeds.section.highlights" =
+  #   #   config.brave.settings.showHighlights or false;
+  #   # Example setting, might not correspond directly to about:config but illustrates the concept
+  #   "browser.newtabpage.activity-stream.feeds.section.highlights" = "true";
+  # };
 }
