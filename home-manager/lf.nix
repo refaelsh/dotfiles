062947a10c3150ed7a85777bc99c2e48786ca40b
refaelsh <refaelsh@pm.me>
2024-09-27 14:00:55 +0300

    WIP

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -1,6 +1,10 @@
+{ pkgs, ... }:
 {
-  # xdg.configFile."lf/icons".source = ./icons;
-
+  xdg.configFile."lf/icons".source = pkgs.fetchurl {
+    url = "https://github.com/gokcehan/lf/blob/master/etc/icons.example";
+    # You might need to specify sha256 or another hash to ensure integrity
+    # sha256 = "your-hash-here";
+  };
   programs.lf = {
     enable = true;
 
