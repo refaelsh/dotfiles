b71587c22a194888d8be1ba6bac331ed129b6821
refaelsh <refaelsh@pm.me>
2024-09-27 14:05:54 +0300

    WIP

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   xdg.configFile."lf/icons".source = pkgs.fetchurl {
-    url = "https://github.com/gokcehan/lf/blob/master/etc/icons.example";
+    url = "https://raw.githubusercontent.com/gokcehan/lf/refs/heads/master/etc/icons.example";
     sha256 = "sha256-20VeJfroHHk6N8oN5Mv0offYFrJXgPaqUYfexLvHv7c=";
   };
   programs.lf = {
