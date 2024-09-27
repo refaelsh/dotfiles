94bc9edf3025274b87b35768e0a4c4a1f0de9943
refaelsh <refaelsh@pm.me>
2024-09-27 17:33:58 +0300

    WIP

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -1,4 +1,23 @@
 { pkgs, ... }:
+let
+  pv = pkgs.writeShellScriptBin "pv.sh" ''
+    file=$1
+    w=$2
+    h=$3
+    x=$4
+    y=$5
+
+    if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+        ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+        exit 1
+    fi
+
+    ${pkgs.pistol}/bin/pistol "$file"
+  '';
+  cl = pkgs.writeShellScriptBin "cl.sh" ''
+    ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+  '';
+in
 {
   xdg.configFile."lf/icons".source = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/gokcehan/lf/master/etc/icons.example";
@@ -13,31 +32,8 @@
       icons = true;
       hidden = true;
       drawbox = true;
+      previewer = "${pv}/bin/pv.sh";
+      cleaner = "${cl}/bin/cl.sh";
     };
-
-    extraConfig =
-      let
-        previewer = pkgs.writeShellScriptBin "pv.sh" ''
-          file=$1
-          w=$2
-          h=$3
-          x=$4
-          y=$5
-
-          if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-              exit 1
-          fi
-
-          ${pkgs.pistol}/bin/pistol "$file"
-        '';
-        cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-          ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
-        '';
-      in
-      ''
-        set cleaner ${cleaner}/bin/clean.sh
-        set previewer ${previewer}/bin/pv.sh
-      '';
   };
 }
