d04388f0307d343e127c3b0a38fcb44571877914
refaelsh <refaelsh@pm.me>
2024-09-27 11:24:24 +0300

    WIP

--- home-manager/main.nix
+++ home-manager/main.nix
@@ -1,18 +1,3 @@
 {
-  home-manager.home.stateVersion = "24.05";
-  home-manager.users.refaelsh = {
-    modules = [
-      ./alacritty.nix
-      ./git.nix
-      ./fzf.nix
-      ./librewolf.nix
-      ./kitty.nix
-      ./mangohud.nix
-      ./starship.nix
-      ./termonad.nix
-      ./wezterm.nix
-      ./xmobar.nix
-      ./zsh.nix
-    ];
-  };
+  home-manager.users.refaelsh = import ./home.nix;
 }
