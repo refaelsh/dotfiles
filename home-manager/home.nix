0fc7c5d5131feefa104ef4cd4a7085bc4666c943
refaelsh <refaelsh@pm.me>
2024-09-29 00:50:21 +0300

    WIP

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,5 +22,14 @@
 
   programs.nixmobar = {
     enable = true;
+    commands = ''
+      Run XMonadLog,
+         Run DiskU [("/", "<fc=#bd93f9><fn=1>\xf0a0</fn></fc> <free>")] [] 50,
+         Run DiskIO [("/", "<read><fc=#bd93f9> R</fc> <fc=#bd93f9>W</fc> <write>")] ["-t", "", "-w", "4"] 10,
+         Run DynNetwork ["-t", "<rx>KB/s<fc=#bd93f9><fn=1>\x1F89B</fn></fc><fc=#bd93f9><fn=1>\x1F899</fn></fc><tx>KB/s", "-w", "5"] 10,
+         Run Memory ["-t", "<fc=#bd93f9><fn=1>\xE266</fn></fc><usedratio>%"] 10,
+         Run Swap ["-t", "<fc=#bd93f9>S</fc><usedratio>%"] 10,
+         Run Kbd [],
+    '';
   };
 }
