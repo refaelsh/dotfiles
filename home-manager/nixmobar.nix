4481a725e6b4045fd35963d7dceb311271e8628d
refaelsh <refaelsh@pm.me>
2024-09-29 00:40:37 +0300

    WIP

--- home-manager/nixmobar.nix
+++ home-manager/nixmobar.nix
@@ -26,6 +26,7 @@ in
       default = {
         bla = "h";
         bla2 = "h2";
+        bla3 = 42;
       };
       description = ''
         An attribute set that will be converted to Xmobar's configuration format.
