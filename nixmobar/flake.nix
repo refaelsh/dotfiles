200773d0bd3cb7328c33ff357e674b4db56f2117
refaelsh <refaelsh@pm.me>
2024-09-29 20:38:17 +0300

    WIP

--- nixmobar/flake.nix
+++ nixmobar/flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs =
