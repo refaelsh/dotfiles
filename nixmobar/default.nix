1c2f768706abe9bb2995937f097913171114ae0a
refaelsh <refaelsh@pm.me>
2024-09-29 20:14:44 +0300

    WIP

--- nixmobar/default.nix
+++ nixmobar/default.nix
@@ -104,27 +104,27 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = {
     home.packages = [ pkgs.xmobar ];
-    # xdg.configFile."xmobar/.xmobarrc" = {
-    #   text = ''
-    #     Config {
-    #       font = "${cfg.font}",
-    #       additionalFonts = [${lib.concatMapStringsSep ", " (s: "\"" + s + "\"") cfg.additionalFonts}],
-    #       bgColor = "${cfg.bgColor}",
-    #       fgColor = "${cfg.fgColor}",
-    #       textOffset = ${toString cfg.textOffset},
-    #       verbose = ${if cfg.verbose then "True" else "False"},
-    #       allDesktops = ${if cfg.allDesktops then "True" else "False"},
-    #       lowerOnStart = ${if cfg.lowerOnStart then "True" else "False"},
-    #       overrideRedirect = ${if cfg.overrideRedirect then "True" else "False"},
-    #       position = ${cfg.position},
-    #       alpha = ${toString cfg.alpha},
-    #       commands = [${cfg.commands}],
-    #       alignSep = "${cfg.alignSep}",
-    #       template = "${cfg.template}"
-    #     }
-    #   '';
-    # };
+    xdg.configFile."xmobar/.xmobarrc" = {
+      text = ''
+        Config {
+          font = "${cfg.font}",
+          additionalFonts = [${lib.concatMapStringsSep ", " (s: "\"" + s + "\"") cfg.additionalFonts}],
+          bgColor = "${cfg.bgColor}",
+          fgColor = "${cfg.fgColor}",
+          textOffset = ${toString cfg.textOffset},
+          verbose = ${if cfg.verbose then "True" else "False"},
+          allDesktops = ${if cfg.allDesktops then "True" else "False"},
+          lowerOnStart = ${if cfg.lowerOnStart then "True" else "False"},
+          overrideRedirect = ${if cfg.overrideRedirect then "True" else "False"},
+          position = ${cfg.position},
+          alpha = ${toString cfg.alpha},
+          commands = [${cfg.commands}],
+          alignSep = "${cfg.alignSep}",
+          template = "${cfg.template}"
+        }
+      '';
+    };
   };
 }
