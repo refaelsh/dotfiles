94785824cc34c3dcd64d27ee02f5599dea740a0d
refaelsh <refaelsh@pm.me>
2024-09-29 22:07:53 +0300

    WIP

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,7 +6,7 @@
   # home.homeDirectory = "/home/refaelsh";
 
   imports = [
-    inputs.nixmobar.homeModules.nixmobar
+    # inputs.nixmobar.homeModules.nixmobar
     ./alacritty.nix
     ./git.nix
     ./fzf.nix
