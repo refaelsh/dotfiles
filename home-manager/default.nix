ac3f62a52f8a1509e465a83ac359942cdc4f2cf1
refaelsh <refaelsh@pm.me>
2024-09-30 21:10:52 +0300

    WIP

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,5 +1,8 @@
-{ nixmobar, ... }:
+{ inputs, nixmobar, ... }:
 {
   # imports = [ nixmobar.homeModules.nixmobar ];
+  extraSpecialArgs = {
+    inherit inputs; # or specific values from inputs
+  };
   home-manager.users.refaelsh = import ./home.nix;
 }
