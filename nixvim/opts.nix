18f2281450f3afe0173c1a08183ce7586ebac28a
refaelsh <refaelsh@pm.me>
2024-10-02 17:40:03 +0300

    WIP

---
+++ nixvim/opts.nix
@@ -0,0 +1,39 @@
+{
+  opts = {
+    guifont = "FiraCode Nerd Font:h8.1";
+    autoread = true;
+    cursorline = true;
+    mouse = "a";
+    undofile = true;
+    undodir = "/home/refaelsh/.config/nvim";
+    swapfile = false;
+    foldmethod = "marker";
+    hlsearch = true;
+    ignorecase = true;
+    completeopt = "menuone,noselect";
+    smartcase = true;
+    incsearch = true;
+    showmatch = true;
+    gdefault = true;
+    termguicolors = true;
+    clipboard = "unnamedplus";
+    tabstop = 4;
+    shiftwidth = 4;
+    expandtab = true;
+    number = true;
+    relativenumber = true;
+    hidden = true;
+    spell = true;
+    spelllang = "en_us";
+    spellcapcheck = "=";
+    spellsuggest = "10";
+    spelloptions = "camel";
+    splitright = true;
+    confirm = true;
+    wrap = false;
+    path.__raw = ''vim.opt.path + "**"'';
+    wildmenu = true;
+    conceallevel = 2;
+    concealcursor = "nc";
+  };
+}
