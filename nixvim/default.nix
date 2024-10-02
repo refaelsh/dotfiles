7d9e57b1e7cda7fdb118f668316d6d0a8a5e5c95
refaelsh <refaelsh@pm.me>
2024-10-02 17:50:55 +0300

    WIP

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -5,6 +5,7 @@
 {
   programs.nixvim = {
     imports = [
+      ./auto-cmd.nix
       ./opts.nix
       ./plugins
     ];
@@ -120,49 +121,6 @@
         mode = "n";
         key = "N";
         action = "Nzzzv";
-      }
-
-    ];
-
-    autoCmd = [
-
-      {
-        event = [
-          "bufenter"
-          "cursorhold"
-          "cursorholdi"
-          "focusgained"
-        ];
-        pattern = [ "*.*" ];
-        command = "if mode() != 'c' | checktime | endif";
-      }
-      {
-        event = [ "FileChangedShellPost" ];
-        pattern = [ "*.*" ];
-        command = "echohl WarningMsg | echo \"File changed on disk. Buffer reloaded.\" | echohl None";
-      }
-
-      {
-        event = [ "QuickFixCmdPost" ];
-        pattern = [ "[^l]*" ];
-        command = "cwindow";
-      }
-      {
-        event = [ "QuickFixCmdPost" ];
-        pattern = [ "l*" ];
-        command = "lwindow";
-      }
-
-      {
-        event = [ "TextYankPost" ];
-        pattern = [ "*" ];
-        command = "silent! lua vim.highlight.on_yank{timeout=300}";
-      }
-
-      {
-        event = [ "BufWritePre" ];
-        pattern = [ "*>8" ];
-        command = ":lua vim.lsp.buf.format()";
       }
 
     ];
