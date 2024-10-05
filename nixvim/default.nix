5d7e2e1ae3b4dc079ab8cf44cfa03ab9a635473e
refaelsh <refaelsh@pm.me>
2024-10-06 00:36:02 +0300

    WIP

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -6,6 +6,7 @@
   programs.nixvim = {
     imports = [
       ./auto-cmd.nix
+      ./keymaps.nix
       ./opts.nix
       ./plugins
     ];
@@ -31,99 +32,6 @@
       maplocalleader = " ";
       have_nerd_font = true;
     };
-
-    keymaps = [
-      {
-        mode = "n";
-        key = ":";
-        action = ";";
-      }
-      {
-        mode = "n";
-        key = ";";
-        action = ":";
-      }
-
-      {
-        mode = "v";
-        key = "p";
-        action = "P";
-      }
-
-      {
-        mode = "n";
-        key = "<leader><space>";
-        action = ":noh<cr>";
-      }
-
-      {
-        mode = "n";
-        key = "<leader>c";
-        action = "gcc";
-        options.remap = true;
-      }
-      {
-        mode = "v";
-        key = "<leader>c";
-        action = "gc";
-        options.remap = true;
-      }
-
-      {
-        mode = "n";
-        key = "j";
-        action = "gj";
-      }
-      {
-        mode = "n";
-        key = "k";
-        action = "gk";
-      }
-
-      {
-        mode = "n";
-        key = "<Enter>";
-        action = "o<ESC>";
-      }
-      {
-        mode = "n";
-        key = "<S-Enter>";
-        action = "O<ESC>";
-      }
-
-      {
-        mode = "n";
-        key = ",t";
-        action = "i#[test]<CR>fn () {<CR>}<ESC>kwi";
-      }
-      {
-        mode = "n";
-        key = ",tm";
-        action = "i#[cfg(test)]<CR>mod tests {<CR>use super::*;<CR><CR>#[test]<CR>fn () {<CR>}<CR><ESC>xxxxi}<ESC>kkwwi";
-      }
-
-      {
-        mode = "n";
-        key = "<C-d>";
-        action = "<C-d>zz";
-      }
-      {
-        mode = "n";
-        key = "<C-u>";
-        action = "<C-u>zz";
-      }
-      {
-        mode = "n";
-        key = "n";
-        action = "nzzzv";
-      }
-      {
-        mode = "n";
-        key = "N";
-        action = "Nzzzv";
-      }
-
-    ];
 
     extraPlugins = with pkgs.vimPlugins; [
       nvim-colorizer-lua
