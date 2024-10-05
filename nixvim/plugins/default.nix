973d6462c6a2bdc604cc3c872f88f70847346761
refaelsh <refaelsh@pm.me>
2024-10-06 00:57:26 +0300

    WIP

--- nixvim/plugins/default.nix
+++ nixvim/plugins/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./cmp.nix
+    ./lsp.nix
     ./one-liners.nix
     ./plugins.nix
   ];
---
+++ nixvim/plugins/lsp.nix
@@ -0,0 +1,35 @@
+{
+  lsp = {
+    enable = true;
+    inlayHints = true;
+    keymaps.lspBuf = {
+      "<leader>d" = "definition";
+      "<leader>a" = "code_action";
+      "<leader>i" = "implementation";
+      "<leader>ic" = "incoming_calls";
+      "<leader>f" = "format";
+      "<leader>h" = "hover";
+      "<leader>r" = "rename";
+    };
+    servers = {
+      nixd.enable = true;
+      yamlls.enable = true;
+      bashls.enable = true;
+      cmake.enable = true;
+      clangd.enable = true;
+      pylsp.enable = true;
+      taplo.enable = true;
+      marksman.enable = true;
+      jsonls.enable = true;
+      hls = {
+        enable = true;
+        filetypes = [
+          "haskell"
+          "lhaskell"
+          "cabal"
+        ];
+      };
+      lua-ls.enable = true;
+    };
+  };
+}
