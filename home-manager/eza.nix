d3b98a0506a26073bb4987418d75ed8c80d5a66b
refaelsh <refaelsh@pm.me>
2024-10-07 21:30:49 +0300

    WIP

--- home-manager/eza.nix
+++ home-manager/eza.nix
@@ -1,7 +1,9 @@
 {
-  home.file.".bla".source = ''
-    sdfsdf
-  '';
+  xdg.configFile."eza/theme.yaml" = {
+    text = ''
+      asd
+    '';
+  };
   programs.eza = {
     enable = true;
     enableZshIntegration = true;
@@ -18,3 +20,25 @@
 }
 
 # "eza -a --icons --long --extended --git --header";
+#   xdg.configFile."xmobar/.xmobarrc" = {
+#     text = # haskell
+#       ''
+#         Config {
+#           font = "${cfg.font}",
+#           additionalFonts = [${lib.concatMapStringsSep ", " (s: "\"" + s + "\"") cfg.additionalFonts}],
+#           bgColor = "${cfg.bgColor}",
+#           fgColor = "${cfg.fgColor}",
+#           textOffset = ${toString cfg.textOffset},
+#           verbose = ${if cfg.verbose then "True" else "False"},
+#           allDesktops = ${if cfg.allDesktops then "True" else "False"},
+#           lowerOnStart = ${if cfg.lowerOnStart then "True" else "False"},
+#           overrideRedirect = ${if cfg.overrideRedirect then "True" else "False"},
+#           position = ${cfg.position},
+#           alpha = ${toString cfg.alpha},
+#           commands = [${cfg.commands}],
+#           alignSep = "${cfg.alignSep}",
+#           template = "${cfg.template}"
+#         }
+#       '';
+#   };
+# };
