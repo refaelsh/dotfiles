75269dc8fd9d7af496d0e1bafca74741428e630e
refaelsh <refaelsh@pm.me>
2024-10-08 18:23:50 +0300

    WIP

--- home-manager/bat.nix
+++ home-manager/bat.nix
@@ -1,6 +1,17 @@
+{ pkgs, ... }:
 {
   programs.bat = {
     enable = true;
-    themes = 
+    themes = {
+      dracula = {
+        src = pkgs.fetchFromGitHub {
+          owner = "dracula";
+          repo = "sublime"; # Bat uses sublime syntax for its themes
+          rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+          sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+        };
+        file = "Dracula.tmTheme";
+      };
+    };
   };
 }
