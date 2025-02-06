c41639c9234e028f114b77ad599173bbc8640f0b
refaelsh <refaelsh@pm.me>
2025-02-06 05:58:49 +0200

    WIP

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -31,19 +31,19 @@
       have_nerd_font = true;
     };
 
-    # extraPlugins = with pkgs.vimPlugins; [
-    #   nvim-colorizer-lua
-    #   nui-nvim
-    #   (pkgs.vimUtils.buildVimPlugin {
-    #     name = "org-bullets.nvim";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "nvim-orgmode";
-    #       repo = "org-bullets.nvim";
-    #       rev = "main";
-    #       sha1 = "M9oUlEa5z7CyQWYFNlW7Am5y+P0=";
-    #     };
-    #   })
-    # ];
+    extraPlugins = with pkgs.vimPlugins; [
+      nvim-colorizer-lua
+      nui-nvim
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "org-bullets.nvim";
+        src = pkgs.fetchFromGitHub {
+          owner = "nvim-orgmode";
+          repo = "org-bullets.nvim";
+          rev = "main";
+          sha1 = "M9oUlEa5z7CyQWYFNlW7Am5y+P0=";
+        };
+      })
+    ];
 
     extraConfigLua = # lua
       ''
