f2edea981009a0e8f30c665837eece642fbb3d9e
refaelsh <refaelsh@pm.me>
2025-02-23 05:51:04 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixmobar.url = "https://codeberg.org/xmobar/xmobar/src/branch/master/nix";
+    nixmobar.url = "git+https://codeberg.org/xmobar/xmobar.git/?dir=nix";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
