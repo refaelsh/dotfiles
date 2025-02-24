8f59a1ac513e5c5833b6d964bc636a1be2221632
refaelsh <refaelsh@pm.me>
2025-02-24 22:36:28 +0200

    WIP

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,7 +15,7 @@
     ./librewolf.nix
     ./kitty.nix
     ./mangohud.nix
-    inputs.nixmobar.homeModules.mainmodule
+    inputs.nixmobar/nix.homeModules.mainmodule
     ./nixmobar.nix
     ./starship.nix
     ./termonad.nix
