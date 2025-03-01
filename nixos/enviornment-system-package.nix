1b36e61bcd263cf9966a771ade84bea26640e9f6
refaelsh <refaelsh@pm.me>
2025-03-01 18:44:22 +0200

    WIP

--- nixos/enviornment-system-package.nix
+++ nixos/enviornment-system-package.nix
@@ -146,7 +146,7 @@
         configFile = pkgs.writeText "config.def.h" (builtins.readFile ./config.h);
 
         # Copy the config.h into the build directory after patching
-        postPatch = oldAttrs.postPatch ++ ''cp ${configFile} config.def.h'';
+        postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
       }))
     ];
   };
