55a80747720dc621ab9f0f003b3189dced81f493
refaelsh <refaelsh@pm.me>
2025-03-01 23:17:55 +0200

    WIP

--- nixos/st-config.h
+++ nixos/st-config.h
@@ -19,7 +19,7 @@ static const char* shell = "/bin/sh";
 char* utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char* scroll = NULL;
-const char* stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
+char* stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
 char* vtiden = "\033[?6c";
