c65902c6b3ee53c7b0e9b7bef0f0ae8cf5ec15ae
refaelsh <refaelsh@pm.me>
2025-03-01 18:53:17 +0200

    WIP

--- nixos/config.h
+++ nixos/config.h
@@ -1,3 +1,5 @@
-static char *font = "DejaVu Sans Mono:pixelsize=14:antialias=true";
+/*static char *font = "DejaVu Sans Mono:pixelsize=14:antialias=true";*/
 static int borderpx = 2;
 static char *shell = "/bin/sh";
+static double minlatency = 2;
+static double maxlatency = 33;
