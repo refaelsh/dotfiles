1a74143f4d77b123df1910b6dd26208d09198be6
refaelsh <refaelsh@pm.me>
2025-03-01 23:02:48 +0200

    WIP

--- nixos/st-config.h
+++ nixos/st-config.h
@@ -14,8 +14,8 @@ static int borderpx = 2;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char* shell = "/bin/sh";
-char* utmp = NULL;
+static const char* shell = "/bin/sh";
+char* utmp = nullptr;
 /* scroll program: to enable use a string like "scroll" */
 char* scroll = NULL;
 char* stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
