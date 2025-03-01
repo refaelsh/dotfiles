793f3123ffbee0d950abb19c1cb9293e76029aa7
refaelsh <refaelsh@pm.me>
2025-03-01 18:34:38 +0200

    WIP

--- nixos/enviornment-system-package.nix
+++ nixos/enviornment-system-package.nix
@@ -142,7 +142,9 @@
       # zen-browser
       (st.overrideAttrs (oldAttrs: rec {
         # Path to your custom config.h
-        configFile = pkgs.writeText "config.h" (builtins.readFile "./config.h");
+
+        # config = builtins.readFile ./xmonad.hs;
+        configFile = pkgs.writeText "config.h" (builtins.readFile ./config.h);
 
         # Copy the config.h into the build directory after patching
         postPatch = ''
