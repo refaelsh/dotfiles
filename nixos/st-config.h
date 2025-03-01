95598003fa3c2a118bc33e734c06683a3d271173
refaelsh <refaelsh@pm.me>
2025-03-01 23:16:57 +0200

    WIP

--- nixos/st-config.h
+++ nixos/st-config.h
@@ -22,7 +22,7 @@ char* scroll = NULL;
 const char* stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
-const char* vtiden = "\033[?6c";
+char* vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
