496aed29192551a1c7cdca45abad89c128028c4e
refaelsh <refaelsh@pm.me>
2025-03-07 14:36:09 +0200

    WIP

--- nixos/enviornment-system-package.nix
+++ nixos/enviornment-system-package.nix
@@ -141,6 +141,13 @@
       shotcut
       (st.overrideAttrs (oldAttrs: rec {
         buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
+        patches = [
+          # ligatures patch
+          (fetchpatch {
+            url = "https://st.suckless.org/patches/ligatures/0.8.3/st-ligatures-20200430-0.8.3.diff";
+            sha256 = "67b668c77677bfcaff42031e2656ce9cf173275e1dfd6f72587e8e8726298f09";
+          })
+        ];
         configFile = pkgs.writeText "config.def.h" (builtins.readFile ./st-config.h);
         postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
       }))
