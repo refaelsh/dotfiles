cfa7a53cbdfa27b3a682b79e7cf1e03ac1dacc29
refaelsh <refaelsh@pm.me>
2025-03-07 17:02:21 +0200

    WIP

--- nixos/st-config.h
+++ nixos/st-config.h
@@ -240,6 +240,8 @@ static uint ignoremod = Mod2Mask | XK_SWITCH_MOD;
  */
 static Key key[] = {
   /* keysym           mask            string      appkey appcursor */
+  { ShiftMask, XK_Page_Up, kscrollup, { .i = -1 } },
+  { ShiftMask, XK_Page_Down, kscrolldown, { .i = -1 } },
   { XK_KP_Home, ShiftMask, "\033[2J", 0, -1 },
   { XK_KP_Home, ShiftMask, "\033[1;2H", 0, +1 },
   { XK_KP_Home, XK_ANY_MOD, "\033[H", 0, -1 },
