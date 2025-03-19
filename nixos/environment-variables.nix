d1b49f758768add551c8bfebcbc628c7289f7433
refaelsh <refaelsh@pm.me>
2025-03-19 21:16:28 +0200

    WIP

--- nixos/environment-variables.nix
+++ nixos/environment-variables.nix
@@ -4,4 +4,7 @@
     TERM = "wezterm";
     TERMINAL = "wezterm";
   };
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1"; # Enables Ozone Wayland support for Chromium-based apps
+  };
 }
