76455f8584c9a6f18471447c331137d4cdde1c44
refaelsh <refaelsh@pm.me>
2025-03-19 19:33:31 +0200

    WIP

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -5,6 +5,10 @@
       "nix-command"
       "flakes"
     ];
+    trusted-users = [
+      "root"
+      "refaelsh"
+    ];
     substituters = [ "https://wezterm.cachix.org" ];
     trusted-public-keys = [ "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0=" ];
   };
