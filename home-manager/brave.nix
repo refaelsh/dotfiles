a1ca4eceb8416f76626eda56145a71f9eacb4e08
refaelsh <refaelsh@pm.me>
2025-04-27 22:33:23 +0300

    WIP

--- home-manager/brave.nix
+++ home-manager/brave.nix
@@ -9,4 +9,22 @@
       "--disable-features=TranslateUI"
     ];
   };
+
+  xdg.configFile."BraveSoftware/Brave-Browser/policies/managed/policies.json".text = ''
+    {
+      "BraveRewardsDisabled": true,
+      "TorDisabled": true,
+      "IPFSDisabled": true,
+      "BraveWalletDisabled": true,
+      "SyncDisabled": true,
+      "PasswordManagerEnabled": false,
+      "AutofillEnabled": false,
+      "SafeBrowsingEnabled": false,
+      "MetricsReportingEnabled": false,
+      "TranslateEnabled": false,
+      "SpellcheckEnabled": false,
+      "ExtensionsToolbarMenuEnabled": false,
+      "HardwareAccelerationMode": 0
+    }
+  '';
 }
