c451d805226266d4b553a43f0e8c32e91f2c96be
refaelsh <refaelsh@pm.me>
2025-04-27 22:32:32 +0300

    WIP

--- home-manager/brave.nix
+++ home-manager/brave.nix
@@ -3,5 +3,10 @@
   programs.chromium = {
     enable = true;
     package = pkgs.brave;
+    commandLineArgs = [
+      "--disable-background-networking"
+      "--disable-default-apps"
+      "--disable-features=TranslateUI"
+    ];
   };
 }
