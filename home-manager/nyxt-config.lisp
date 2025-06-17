805c9a2a3de9d08cb21d5f3f5018d454881a6e60
refaelsh <refaelsh@pm.me>
2025-06-17 21:15:19 +0300

    WIP

--- home-manager/nyxt-config.lisp
+++ home-manager/nyxt-config.lisp
@@ -1,17 +1,13 @@
-; (define-configuration browser
-;   ((theme theme:+dark-theme+)))
-;
-; (define-configuration nyxt/mode/style:dark-mode
-;   ((style (theme:themed-css (theme *browser*)
-;             `(* :background-color ,theme:background "!important"
-;                 :color ,theme:text "!important")))))
-;
-; (define-configuration buffer
-;   ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))
-;    (search-engines (list (make-instance 'search-engine
-;                                        :shortcut "g"
-;                                        :search-url "https://www.google.com/search?q=~a"
-;                                        :fallback-url "https://www.google.com")))))
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration buffer
+  ((default-modes (append '(nyxt/mode/style:dark-mode) %slot-default%))
+   (search-engines (list (make-instance 'search-engine
+                                       :shortcut "g"
+                                       :search-url "https://www.google.com/search?q=~a"
+                                       :fallback-url "https://www.google.com")))))
 
 (define-configuration web-buffer
-  ( (web-engine :blink)))
+  ((default-new-buffer-url "about:blank")
+   (web-engine :blink)))
