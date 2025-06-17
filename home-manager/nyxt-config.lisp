cbdc29c6d25ca00d4435240ead90530d35950883
refaelsh <refaelsh@pm.me>
2025-06-17 21:03:22 +0300

    WIP

--- home-manager/nyxt-config.lisp
+++ home-manager/nyxt-config.lisp
@@ -1,4 +1,17 @@
-; (in-package :nyxt-user)
-;
-; (define-configuration browser
-;   ((renderer :blink)))
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration nyxt/mode/style:dark-mode
+  ((style (theme:themed-css (theme *browser*)
+            `(* :background-color ,theme:background "!important"
+                :color ,theme:text "!important")))))
+
+(define-configuration buffer
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))
+   (search-engines (list (make-instance 'search-engine
+                                       :shortcut "g"
+                                       :search-url "https://www.google.com/search?q=~a"
+                                       :fallback-url "https://www.google.com")))))
+
+(define-configuration web-buffer
+  ((renderer (make-instance 'qt-web-engine))))
