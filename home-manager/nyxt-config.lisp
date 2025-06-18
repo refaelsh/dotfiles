7bb37ea7b2c7a4c5081bbbd2612bc3344d0d7c94
refaelsh <refaelsh@pm.me>
2025-06-18 18:38:35 +0300

    WIP

--- home-manager/nyxt-config.lisp
+++ home-manager/nyxt-config.lisp
@@ -1,12 +1,12 @@
-(define-configuration browser
-  ((theme theme:+dark-theme+)))
-
-(define-configuration buffer
-  ((default-modes (append '(nyxt/mode/style:dark-mode) %slot-default%))
-   (search-engines (list (make-instance 'search-engine
-                                       :shortcut "g"
-                                       :search-url "https://www.google.com/search?q=~a"
-                                       :fallback-url "https://www.google.com")))))
+; (define-configuration browser
+;   ((theme theme:+dark-theme+)))
+;
+; (define-configuration buffer
+;   ((default-modes (append '(nyxt/mode/style:dark-mode) %slot-default%))
+;    (search-engines (list (make-instance 'search-engine
+;                                        :shortcut "g"
+;                                        :search-url "https://www.google.com/search?q=~a"
+;                                        :fallback-url "https://www.google.com")))))
 
 (define-configuration web-buffer
   ((default-new-buffer-url "about:blank")
--- home-manager/nyxt.nix
+++ home-manager/nyxt.nix
@@ -1,9 +1,14 @@
-{ pkgs, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   # programs.nyxt = {
   #   enable = true;
   # };
 
   # home.file.".config/nyxt/config.lisp".source = ./nyxt-config.lisp;
-  xdg.configFile."nyxt/config.lisp".source = ./nyxt-config.lisp;
+  xdg.configFile."nyxt/config.lisp".source = config.lib.file.mkOutOfStoreSymlink "./nyxt-config.lisp";
 }
