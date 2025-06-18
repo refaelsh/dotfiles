584eca28e9accf3ccbf53a5ba8d7120188febb61
refaelsh <refaelsh@pm.me>
2025-06-18 20:07:32 +0300

    WIP

--- home-manager/nyxt.nix
+++ home-manager/nyxt.nix
@@ -1,20 +1,9 @@
-{
-  config,
-  pkgs,
-  inputs,
-  ...
-}:
-let
-  configDir = builtins.dirOf (builtins.toString ./.);
-in
+{ pkgs, inputs, ... }:
 {
   # programs.nyxt = {
   #   enable = true;
   # };
 
   # home.file.".config/nyxt/config.lisp".source = ./nyxt-config.lisp;
-  xdg.configFile."nyxt/config.lisp" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${configDir}/home-manager/nyxt-config.lisp";
-    onChange = "";
-  };
+  xdg.configFile."nyxt/config.lisp".source = ./nyxt-config.lisp;
 }
