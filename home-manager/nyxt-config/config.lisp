5e9eae2dfe97df9a750bf0c227c87f29a345bb84
refaelsh <refaelsh@pm.me>
2025-06-18 22:12:47 +0300

    WIP

--- home-manager/nyxt-config/config.lisp
+++ home-manager/nyxt-config/config.lisp
@@ -4,8 +4,8 @@
 (nyxt::load-lisp "./statusline.lisp")
 (nyxt::load-lisp "./stylesheet.lisp")
 
-(define-configuration browser
-  ((theme theme:+dark-theme+)))
+; (define-configuration browser
+;   ((theme theme:+dark-theme+)))
 
 (define-configuration web-buffer
   ((web-engine :blink)))
