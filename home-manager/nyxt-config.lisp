2e6a129429f1714e22eaf9745a2c54675633fadc
refaelsh <refaelsh@pm.me>
2025-06-18 07:32:55 +0300

    WIP

--- home-manager/nyxt-config.lisp
+++ home-manager/nyxt-config.lisp
@@ -1,13 +1,17 @@
 (define-configuration browser
   ((theme theme:+dark-theme+)))
 
+(define-configuration nyxt/mode/style:dark-mode
+  ((style (theme:themed-css (theme *browser*)
+            `(* :background-color ,theme:background "!important"
+                :color ,theme:text "!important")))))
+
 (define-configuration buffer
-  ((default-modes (append '(nyxt/mode/style:dark-mode) %slot-default%))
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))
    (search-engines (list (make-instance 'search-engine
                                        :shortcut "g"
                                        :search-url "https://www.google.com/search?q=~a"
                                        :fallback-url "https://www.google.com")))))
 
 (define-configuration web-buffer
-  ((default-new-buffer-url "about:blank")
-   (web-engine :blink)))
+  ( (web-engine :blink)))
