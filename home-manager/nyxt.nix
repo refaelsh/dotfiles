319dfa24ca157e9bd2379cf14432f801b4566e15
refaelsh <refaelsh@pm.me>
2025-06-18 18:50:36 +0300

    WIP

--- home-manager/nyxt.nix
+++ home-manager/nyxt.nix
@@ -4,11 +4,15 @@
   inputs,
   ...
 }:
+let
+  configDir = builtins.dirOf (builtins.toString ./.);
+in
 {
   # programs.nyxt = {
   #   enable = true;
   # };
 
   # home.file.".config/nyxt/config.lisp".source = ./nyxt-config.lisp;
-  xdg.configFile."nyxt/config.lisp".source = config.lib.file.mkOutOfStoreSymlink "nyxt-config.lisp";
+  xdg.configFile."nyxt/config.lisp".source =
+    config.lib.file.mkOutOfStoreSymlink "${configDir}/nyxt-config.lisp";
 }
