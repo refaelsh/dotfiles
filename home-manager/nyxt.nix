8993fe10ad6c238cf7c67d44c662ccd62e5a399d
refaelsh <refaelsh@pm.me>
2025-06-18 22:27:56 +0300

    WIP

--- home-manager/nyxt.nix
+++ home-manager/nyxt.nix
@@ -6,4 +6,6 @@
 
   # home.file.".config/nyxt/config.lisp".source = ./nyxt-config.lisp;
   xdg.configFile."nyxt/config.lisp".source = ./nyxt-config/config.lisp;
+  xdg.configFile."nyxt/statusline.lisp".source = ./nyxt-config/statusline.lisp;
+  xdg.configFile."nyxt/stylesheet.lisp".source = ./nyxt-config/stylesheet.lisp;
 }
