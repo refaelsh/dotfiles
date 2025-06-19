526b407e3d40acfcbdfc46d8dc60904c6cf6b60a
refaelsh <refaelsh@pm.me>
2025-06-19 20:03:04 +0300

    WIP

--- home-manager/nyxt-config/config.lisp
+++ home-manager/nyxt-config/config.lisp
@@ -3,22 +3,22 @@
 ; (nyxt::load-lisp "./statusline.lisp")
 ; (nyxt::load-lisp "./stylesheet.lisp")
 
-(define-configuration web-buffer
-  ((default-modes (pushnew 'nyxt/force-https-mode:force-https-mode %slot-value%))))
-
-(define-configuration browser
-  ((default-modes (append '(force-https-mode) %slot-default%))))
+; (define-configuration web-buffer
+;   ((default-modes (pushnew 'nyxt/force-https-mode:force-https-mode %slot-value%))))
+;
+; (define-configuration browser
+;   ((default-modes (append '(force-https-mode) %slot-default%))))
 
 (define-configuration status-buffer ((glyph-mode-presentation-p t)))
 (define-configuration nyxt/force-https-mode:force-https-mode ((glyph "")))
-(define-configuration nyxt/blocker-mode:blocker-mode ((glyph "")))
-(define-configuration nyxt/proxy-mode:proxy-mode ((glyph "")))
-(define-configuration nyxt/reduce-tracking-mode:reduce-tracking-mode  ((glyph "")))
-(define-configuration nyxt/certificate-exception-mode:certificate-exception-mode ((glyph "")))
-(define-configuration nyxt/style-mode:style-mode ((glyph "")))
-(define-configuration nyxt/help-mode:help-mode ((glyph "")))
-(define-configuration nyxt/web-mode:web-mode ((glyph "ω")))
-(define-configuration nyxt/auto-mode:auto-mode ((glyph "α")))
+; (define-configuration nyxt/blocker-mode:blocker-mode ((glyph "")))
+; (define-configuration nyxt/proxy-mode:proxy-mode ((glyph "")))
+; (define-configuration nyxt/reduce-tracking-mode:reduce-tracking-mode  ((glyph "")))
+; (define-configuration nyxt/certificate-exception-mode:certificate-exception-mode ((glyph "")))
+; (define-configuration nyxt/style-mode:style-mode ((glyph "")))
+; (define-configuration nyxt/help-mode:help-mode ((glyph "")))
+; (define-configuration nyxt/web-mode:web-mode ((glyph "ω")))
+; (define-configuration nyxt/auto-mode:auto-mode ((glyph "α")))
 
 ; (define-configuration browser
 ;   ((theme (make-instance
