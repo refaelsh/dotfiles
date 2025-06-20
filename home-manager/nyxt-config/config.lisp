1d7f88c131c7b9a9fc771bb9b7b96b8fd82871c1
refaelsh <refaelsh@pm.me>
2025-06-20 08:21:49 +0300

    WIP

--- home-manager/nyxt-config/config.lisp
+++ home-manager/nyxt-config/config.lisp
@@ -9,8 +9,12 @@
 ; (define-configuration browser
 ;   ((default-modes (append '(force-https-mode) %slot-default%))))
 
-(define-configuration status-buffer ((glyph-mode-presentation-p t)))
-(define-configuration nyxt/force-https-mode:force-https-mode ((glyph "")))
+; (define-configuration status-buffer ((glyph-mode-presentation-p t)))
+; (define-configuration nyxt/force-https-mode:force-https-mode ((glyph "")))
+
+(define-configuration nyxt/mode/force-https:force-https-mode
+  ((glyph "")))
+
 ; (define-configuration nyxt/blocker-mode:blocker-mode ((glyph "")))
 ; (define-configuration nyxt/proxy-mode:proxy-mode ((glyph "")))
 ; (define-configuration nyxt/reduce-tracking-mode:reduce-tracking-mode  ((glyph "")))
