996d7bec465bed78c6924e89ee1da58768711e8d
refaelsh <refaelsh@pm.me>
2025-06-20 17:30:59 +0300

    WIP

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   hardware = {
     enableRedistributableFirmware = true;
@@ -8,6 +9,11 @@
     graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        libvdpau-va-gl
+      ];
     };
     # keyboard.zsa.enable = true;
   };
