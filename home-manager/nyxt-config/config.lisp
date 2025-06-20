b73100c48dbdc5c861c475751eea3417fbf1952e
refaelsh <refaelsh@pm.me>
2025-06-20 15:28:44 +0300

    WIP

--- home-manager/nyxt-config/config.lisp
+++ home-manager/nyxt-config/config.lisp
@@ -44,5 +44,5 @@
 ; (define-configuration browser
 ;   ((theme theme:+dark-theme+)))
 
-; (define-configuration web-buffer
-;   ((web-engine :blink)))
+(define-configuration web-buffer
+  ((web-engine :blink)))
