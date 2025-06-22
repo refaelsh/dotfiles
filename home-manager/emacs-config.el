30b19d5592d92af0fac1841c727cdfa3efa23bc3
refaelsh <refaelsh@pm.me>
2025-06-22 23:20:39 +0300

    WIP

--- home-manager/emacs-config.el
+++ home-manager/emacs-config.el
@@ -1,1 +1,22 @@
-(load-theme 'wombat t)
+(load-theme 'wombat)
+
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(menu-bar-mode -1)
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "FiraCode Nerd Font Ret" :height 130)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
