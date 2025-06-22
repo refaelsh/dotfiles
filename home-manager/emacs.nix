093ba937503beae660258c0db4e7cc90fc204ea7
refaelsh <refaelsh@pm.me>
2025-06-22 21:40:27 +0300

    WIP

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -1,3 +1,5 @@
+{ pkgs, inputs, ... }:
 {
-  xdg.configFile."emacs/init.el".source = ./init.el;
+  # xdg.configFile."emacs/init.el".source = ./init.el;
+  home.file.".emacs.d/init.el".source = ./init.el;
 }
