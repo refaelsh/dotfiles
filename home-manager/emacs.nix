fec091d115445ab26c1ecf5593eaae3239946fcf
refaelsh <refaelsh@pm.me>
2025-06-25 11:19:52 +0300

    WIP

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -1,20 +1,20 @@
 {
-  # home.file.".emacs.d/init.el" = {
-  #   text = # lisp
-  #     ''
-  #       (org-babel-load-file
-  #        ; (expand-file-name
-  #         "~/repos/dotfiles/home-manager/emacs-config.org"
-  #     ; user-emacs-directory)
-  #     )
-  #     '';
-  # };
-  # home.file.".emacs.d/early-init.el" = {
-  #   text = # lisp
-  #     ''
-  #       (push '(menu-bar-lines . 0) default-frame-alist)
-  #       (push '(tool-bar-lines . 0) default-frame-alist)
-  #       (push '(vertical-scroll-bars) default-frame-alist)
-  #     '';
-  # };
+  home.file.".emacs.d/init.el" = {
+    text = # lisp
+      ''
+        (org-babel-load-file
+         ; (expand-file-name
+          "~/repos/dotfiles/home-manager/emacs-config.org"
+      ; user-emacs-directory)
+      )
+      '';
+  };
+  home.file.".emacs.d/early-init.el" = {
+    text = # lisp
+      ''
+        (push '(menu-bar-lines . 0) default-frame-alist)
+        (push '(tool-bar-lines . 0) default-frame-alist)
+        (push '(vertical-scroll-bars) default-frame-alist)
+      '';
+  };
 }
