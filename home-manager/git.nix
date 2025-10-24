0f217f14b8497e34e47482aea7e6d4c334cce477
refaelsh <refaelsh@pm.me>
2025-10-24 14:17:28 +0300

    WIP

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,6 +1,7 @@
 {
   programs.git = {
     enable = true;
+    settings = {};
     aliases = {
       lg = "log --date-order --color-moved --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
       st = "status";
