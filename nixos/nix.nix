5c734b5aadb011f1c673881b42573117648ea732
refaelsh <refaelsh@pm.me>
2026-03-21 19:54:04 +0200

    WIP

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,19 +1,19 @@
 {
-nix = {
-            daemonCPUSchedPolicy = "idle";
-            daemonIOSchedClass   = "idle";
-          };
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    trusted-users = [
-      "root"
-      "refaelsh"
-    ];
-    substituters = [ "https://wezterm.cachix.org" ];
-    trusted-public-keys = [ "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0=" ];
+  nix = {
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedClass   = "idle";
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "refaelsh"
+      ];
+      substituters = [ "https://wezterm.cachix.org" ];
+      trusted-public-keys = [ "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0=" ];
+    };
   };
 }
