b57ca6e619f04ec2167a595fe2c7077abdab1714
refaelsh <refaelsh@pm.me>
2026-03-31 18:52:44 +0300

    WIP

--- modules/programs/brave/default.nix
+++ modules/programs/brave/default.nix
@@ -3,17 +3,15 @@
   flake.nixosModules.brave =
     { config, lib, ... }:
     {
-      # Exact same Brave config you had in Home-Manager, but now as a NixOS dendritic module
-      programs.chromium = {
-        enable = true;
-        package = pkgs.brave;
-
-        # ← This is the NixOS name (was commandLineArgs in Home-Manager)
-        extraArgs = [
-          "--disable-background-networking"
-          "--disable-default-apps"
-          "--disable-features=TranslateUI"
-        ];
-      };
+      # Exact same flags you had in Home-Manager, but now as a proper NixOS dendritic feature
+      environment.systemPackages = [
+        (pkgs.brave.override {
+          commandLineArgs = [
+            "--disable-background-networking"
+            "--disable-default-apps"
+            "--disable-features=TranslateUI"
+          ];
+        })
+      ];
     };
 }
