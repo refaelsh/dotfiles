aeb4793febd6dce8fb14adea24ff2e321611ab83
refaelsh <refaelsh@pm.me>
2026-04-01 16:28:55 +0300

    WIP

--- modules/programs/bat.nix
+++ modules/programs/bat.nix
@@ -1,17 +1,22 @@
 { inputs, ... }:
-
 {
   # Correct flake-parts wrapper (this is what makes it dendritic)
   flake.nixosModules.bat =
     { pkgs, ... }:
+    let
       bat-wrapped = inputs.wrappers.lib.wrapPackage {
         inherit pkgs;
 
         package = pkgs.bat;
 
-        # Defaults (you can still override any of these in your Home-Manager config)
+        # Defaults that match your Kitty Dracula theme
         env = {
-          BAT_THEME = "Dracula"; # matches your Kitty theme
+          BAT_THEME = "Dracula";
+          BAT_PAGING = "auto"; # or "never" / "always" if you prefer
         };
-    }
+      };
+    in
+    {
+      environment.systemPackages = [ bat-wrapped ];
+    };
 }
