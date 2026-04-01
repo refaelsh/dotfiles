30b2285d6d4ed462b83ea198ac097d29d06fcd7d
refaelsh <refaelsh@pm.me>
2026-04-01 17:47:14 +0300

    WIP

--- modules/programs/mangohud.nix
+++ modules/programs/mangohud.nix
@@ -1,8 +1,7 @@
 { inputs, ... }:
 {
   # Simple dendritic feature — matches your Home-Manager config 1:1
-  flake.nixosModules.mangohud =
-    { ... }:
+  flake.nixosModules.mangohud = { ... }:
     {
       programs.mangohud = {
         enable = true;
