4c750ac7ebf2a71fccf26bd338a6dc8e3bb1a08a
refaelsh <refaelsh@pm.me>
2026-04-01 13:14:45 +0300

    WIP

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,8 +3,14 @@
   # This is the real dendritic magic:
   # Every file in modules/ that exports flake.nixosModules.<name>
   # is automatically included. No editing needed ever again.
-  flake.nixosModules.nixos = { config, lib, pkgs, ... }:
-  {
-    imports = lib.attrValues (removeAttrs inputs.self.nixosModules [ "nixos" ]);
-  };
+  flake.nixosModules.nixos =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = lib.attrValues (removeAttrs inputs.self.nixosModules [ "nixos" ]);
+    };
 }
