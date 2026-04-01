8615c8cb0a3259ff16e0c5375ad377dc4036f463
refaelsh <refaelsh@pm.me>
2026-04-01 14:03:22 +0300

    WIP

--- flake.nix
+++ flake.nix
@@ -3,27 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixmobar.url = "git+https://codeberg.org/xmobar/xmobar.git/?dir=nix";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    host = import ./modules/hosts/refael-nixos.nix {
-      inherit inputs;
-      inherit (nixpkgs) lib;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ (inputs.import-tree ./modules) ];
+
+      # Your host is on x86_64-linux, adjust if needed
+      systems = [ "x86_64-linux" ];
     };
-  in
-  {
-    # Both the modules and the configuration now come from the single unified file
-    nixosModules = host.flake.nixosModules;
-    nixosConfigurations = host.flake.nixosConfigurations;
-  };
 }
