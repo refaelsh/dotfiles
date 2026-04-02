606c692de38a3d99b996a1d423c1188767ccca74
refaelsh <refaelsh@pm.me>
2026-04-02 22:30:22 +0300

    WIP

--- modules/programs/nixvim/nixvim.nix
+++ modules/programs/nixvim/nixvim.nix
@@ -1,50 +1,56 @@
 { inputs, ... }:
 {
-  flake.nixosModules.nixvim = { config, lib, pkgs, ... }:
-  {
-    # Pull in the official nixvim base module
-    imports = [ inputs.nixvim.nixosModules.nixvim ];
+  flake.nixosModules.nixvim =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      # Pull in the official nixvim base module
+      imports = [ inputs.nixvim.nixosModules.nixvim ];
 
-    programs.nixvim = {
-      imports = [
-        "${inputs.self}/modules/nixvim/_auto-cmd.nix"
-        "${inputs.self}/modules/nixvim/_keymaps.nix"
-        "${inputs.self}/modules/nixvim/_opts.nix"
-        "${inputs.self}/modules/nixvim/_plugins"
-      ];
+      programs.nixvim = {
+        imports = [
+          ./_auto-cmd.nix
+          ./_keymaps.nix
+          ./_opts.nix
+          ./_plugins
+        ];
 
-      enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-      viAlias = true;
-      enableMan = true;
-      colorschemes.dracula-nvim.enable = true;
-
-      performance.byteCompileLua = {
         enable = true;
-        configs = true;
-        initLua = true;
-        nvimRuntime = true;
-        plugins = true;
+        defaultEditor = true;
+        vimAlias = true;
+        viAlias = true;
+        enableMan = true;
+        colorschemes.dracula-nvim.enable = true;
+
+        performance.byteCompileLua = {
+          enable = true;
+          configs = true;
+          initLua = true;
+          nvimRuntime = true;
+          plugins = true;
+        };
+
+        globals = {
+          mapleader = " ";
+          maplocalleader = " ";
+          have_nerd_font = true;
+        };
+
+        extraPlugins = with pkgs.vimPlugins; [
+          vim-nix
+        ];
+
+        extraConfigLua = # lua
+          ''
+            -- require('org-bullets').setup()
+            -- require('colorizer').setup({
+            --   -- mode = 'background'
+            -- })
+          '';
       };
-
-      globals = {
-        mapleader = " ";
-        maplocalleader = " ";
-        have_nerd_font = true;
-      };
-
-      extraPlugins = with pkgs.vimPlugins; [
-        vim-nix
-      ];
-
-      extraConfigLua = # lua
-        ''
-          -- require('org-bullets').setup()
-          -- require('colorizer').setup({
-          --   -- mode = 'background'
-          -- })
-        '';
     };
-  };
 }
