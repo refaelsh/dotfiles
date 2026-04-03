218b992e8d262dcde992d5cb4ac19d18ffd7e4ef
refaelsh <refaelsh@pm.me>
2026-04-03 12:46:51 +0300

    WIP

--- modules/programs/waybar.nix
+++ modules/programs/waybar.nix
@@ -1,33 +1,34 @@
 { inputs, ... }:
-
 {
   # Clean dendritic feature using official Lassulus/wrappers waybar module
-  flake.nixosModules.waybar = { pkgs, ... }:
+  flake.nixosModules.waybar =
+    { pkgs, ... }:
     let
-      waybar-wrapped = (inputs.wrappers.wrapperModules.waybar.apply {
-        inherit pkgs;
+      waybar-wrapped =
+        (inputs.wrappers.wrapperModules.waybar.apply {
+          inherit pkgs;
 
-        settings = {
-          mainBar = {
-            layer = "top";
-            position = "top";
-            height = 32;
-            margin = "4 4 0 4";
+          settings = {
+            mainBar = {
+              layer = "top";
+              position = "top";
+              height = 32;
+              margin = "4 4 0 4";
+            };
           };
-        };
 
-        # This is the ONLY correct way to set the style
-        style.css = ''
-          * {
-            font-family: "Fira Code", "FiraCode Nerd Font";
-            font-size: 13px;
-          }
-          window#waybar {
-            background: #282A36;
-            color: #F8F8F2;
-          }
-        '';
-      }).wrapper;
+          # This is the ONLY correct way to set the style
+          style = ''
+            * {
+              font-family: "Fira Code", "FiraCode Nerd Font";
+              font-size: 13px;
+            }
+            window#waybar {
+              background: #282A36;
+              color: #F8F8F2;
+            }
+          '';
+        }).wrapper;
     in
     {
       environment.systemPackages = [ waybar-wrapped ];
