d43ff7fcb44fb5a59d38c71088f895f95973628a
refaelsh <refaelsh@pm.me>
2026-04-03 12:04:29 +0300

    WIP

---
+++ modules/programs/waybar.nix
@@ -0,0 +1,35 @@
+{ inputs, ... }:
+
+{
+  # Clean dendritic feature using official Lassulus/wrappers waybar module
+  flake.nixosModules.waybar = { pkgs, ... }:
+    let
+      waybar-wrapped = (inputs.wrappers.wrapperModules.waybar.apply {
+        inherit pkgs;
+
+        settings = {
+          mainBar = {
+            layer = "top";
+            position = "top";
+            height = 32;
+            margin = "4 4 0 4";
+          };
+        };
+
+        # This is the ONLY correct way to set the style
+        style.css = ''
+          * {
+            font-family: "Fira Code", "FiraCode Nerd Font";
+            font-size: 13px;
+          }
+          window#waybar {
+            background: #282A36;
+            color: #F8F8F2;
+          }
+        '';
+      }).wrapper;
+    in
+    {
+      environment.systemPackages = [ waybar-wrapped ];
+    };
+}
