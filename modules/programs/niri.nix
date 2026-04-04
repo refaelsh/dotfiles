7222a4a58bea517f541788cc5190b79e3d554aa9
refaelsh <refaelsh@pm.me>
2026-04-04 21:07:49 +0300

    WIP

--- modules/programs/niri.nix
+++ modules/programs/niri.nix
@@ -1,36 +1,39 @@
 { inputs, ... }:
-
 {
   # Dendritic feature using the official Lassulus/wrappers niri module
-  # Mod + Enter opens kitty (pure Nix attrset, no raw KDL)
-  flake.nixosModules.niri = { pkgs, ... }:
+  # Mod + Enter opens kitty
+  flake.nixosModules.niri =
+    { pkgs, ... }:
     let
-      niri-wrapped = (inputs.wrappers.wrapperModules.niri.apply {
-        inherit pkgs;
+      niri-wrapped =
+        (inputs.wrappers.wrapperModules.niri.apply {
+          inherit pkgs;
 
-        settings = {
-          spawn-at-startup = [
-            "waybar"
-          ];
+          # Raw KDL string (the reliable way to avoid KDL parsing issues with this wrapper)
+          settings = ''
+            spawn-at-startup = [
+              "waybar"
+            ]
 
-          input.keyboard.xkb = {
-            layout = "us";
-          };
+            input "keyboard" {
+              xkb {
+                layout = "us"
+              }
+            }
 
-          prefer-no-csd = true;
+            prefer-no-csd = true
 
-          cursor = {
-            hide-when-typing = true;
-          };
+            cursor {
+              hide-when-typing = true
+            }
 
-          # Mod + Enter opens kitty
-          binds = {
-            "Mod+Enter" = {
-              spawn = [ "kitty" ];
-            };
-          };
-        };
-      }).wrapper;
+            binds {
+              "Mod+Enter" {
+                spawn "kitty"
+              }
+            }
+          '';
+        }).wrapper;
     in
     {
       environment.systemPackages = [ niri-wrapped ];
