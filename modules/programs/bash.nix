399797569f13df1992218d34c46106b2fd8f971a
refaelsh <refaelsh@pm.me>
2026-04-05 23:05:37 +0300

    WIP

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
   # Dendritic bash feature – pure NixOS, no Home-Manager
-  # interactiveShellInit now runs on every Kitty window thanks to the -i flag above
+  # interactiveShellInit is now enough because Kitty launches with -i
   flake.nixosModules.bash =
     { pkgs, lib, ... }:
     {
