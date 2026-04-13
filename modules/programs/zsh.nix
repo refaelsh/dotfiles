542e002bf3749d3274a3f368f52055c61527287c
refaelsh <refaelsh@pm.me>
2026-04-13 19:55:38 +0300

    WIP

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -43,16 +43,6 @@
             fpath+=(${pkgs.zsh-completions}/share/zsh/site-functions)
             source ${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh
 
-            # Oh-My-Zsh (still the easiest way to keep your exact plugin list)
-            export ZSH="${pkgs.oh-my-zsh}/share/oh-my-zsh"
-            plugins=(
-              sudo git git-extras git-escape-magic gitfast zsh-interactive-cd
-              vi-mode colored-man-pages extract cp cabal
-              # fzf is now handled by the wrapper's integration — you can drop it here if you want
-            )
-
-            source $ZSH/oh-my-zsh.sh
-
             # Dracula theme for zsh-syntax-highlighting (exact copy of what you had)
             typeset -gA ZSH_HIGHLIGHT_STYLES
             ZSH_HIGHLIGHT_STYLES[comment]='fg=#6272A4'
@@ -143,6 +133,24 @@
             "cursor"
             "root"
             "line"
+          ];
+        };
+
+        # ← Oh My Zsh is now declarative (moved out of extraRC)
+        ohMyZsh = {
+          enable = true;
+          plugins = [
+            "sudo"
+            "git"
+            "git-extras"
+            "git-escape-magic"
+            "gitfast"
+            "zsh-interactive-cd"
+            "vi-mode"
+            "colored-man-pages"
+            "extract"
+            "cp"
+            "cabal"
           ];
         };
       };
