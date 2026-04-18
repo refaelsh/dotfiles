3f6d936f1ddd316dc79d6b8ca7e761e73ba29152
refaelsh <refaelsh@pm.me>
2026-04-18 21:09:42 +0300

    WIP

--- modules/programs/bat.nix
+++ modules/programs/bat.nix
@@ -4,27 +4,25 @@
   flake.nixosModules.bat =
     { pkgs, ... }:
     let
-      bat-wrapped = inputs.wrappers.lib.wrapPackage {
-        inherit pkgs;
-
-        package = pkgs.bat;
-
-        flags = {
-          "--style" = "header,grid,numbers,changes,snip";
-          "--theme" = "Dracula";
-          "--pager" = "less --RAW-CONTROL-CHARS";
-        };
-      };
+      bat-wrapped =
+        (inputs.wrappers.wrapperModules.bat.apply {
+          inherit pkgs;
+          "bat-config".content = ''
+            # bat configuration (migrated from old wrapPackage flags)
+            # see: https://github.com/sharkdp/bat/blob/master/README.md#configuration-file
+            style = header,grid,numbers,changes,snip
+            theme = Dracula
+            pager = "less --RAW-CONTROL-CHARS"
+          '';
+        }).wrapper;
     in
     {
       environment = {
         shellAliases = {
           # cat = "bat";
         };
-
         # systemPackages = [ bat-wrapped ];
       };
-
       programs.bat = {
         enable = true;
         package = bat-wrapped;
