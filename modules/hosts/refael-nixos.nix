4408e430f3821ba77b22bd470f0f5281dd9c5395
refaelsh <refaelsh@pm.me>
2026-04-22 21:28:37 +0300

    WIP

--- modules/hosts/refael-nixos.nix
+++ modules/hosts/refael-nixos.nix
@@ -21,21 +21,21 @@
       # One line = all your features (brave, kitty, starship, git, etc.) are pulled in automatically
       inputs.self.nixosModules.nixos
 
-      # # KBDD overlay
-      # {
-      #   nixpkgs.overlays = [
-      #     (final: prev: {
-      #       kbdd = prev.kbdd.overrideAttrs (old: {
-      #         src = prev.fetchFromGitHub {
-      #           owner = "qnikst";
-      #           repo = "kbdd";
-      #           rev = "b87e44afd5859157245eee22b11827605bfa09b9";
-      #           hash = "sha256-cbMcB6jgssfMUjemBOiE06zJK2TbzOWt1Rvt41V33Mo=";
-      #         };
-      #       });
-      #     })
-      #   ];
-      # }
+      # KBDD overlay
+      {
+        nixpkgs.overlays = [
+          (final: prev: {
+            kbdd = prev.kbdd.overrideAttrs (old: {
+              src = prev.fetchFromGitHub {
+                owner = "qnikst";
+                repo = "kbdd";
+                rev = "b87e44afd5859157245eee22b11827605bfa09b9";
+                hash = "sha256-cbMcB6jgssfMUjemBOiE06zJK2TbzOWt1Rvt41V33Mo=";
+              };
+            });
+          })
+        ];
+      }
     ];
   };
 }
